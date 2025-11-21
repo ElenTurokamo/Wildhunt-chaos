8e209c7bbed5231da73a08200878ac5f71b1c010
ElenTurokamo <117432225+ElenTurokamo@users.noreply.github.com>
2025-11-21 21:39:39 +0500

    Доработки пользовательского интерфейса

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,40 +2,38 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public GameObject gameOverUI;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("UI")]
+    [SerializeField] private GameObject gameplayUI;  
+    [SerializeField] private GameObject gameOverUI; 
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Bullet bullet = collision.GetComponent<Bullet>();
-        if (bullet != null)
+        if (bullet != null && bullet.isEnemy)
         {
-            if (bullet.isEnemy)
-            {
-                gameOverUI.SetActive(true);
-                Time.timeScale = 0f;
-                Destroy(gameObject);
-                Destroy(bullet.gameObject);
-            }
+            PlayerDie();
+            Destroy(bullet.gameObject);
+            return;
         }
 
         EnemyDestructable destructable = collision.GetComponent<EnemyDestructable>();
         if (destructable != null)
         {
-            gameOverUI.SetActive(true);
-            Time.timeScale = 0f;
-            Destroy(gameObject);    
-            Destroy(destructable.gameObject);    
+            PlayerDie();
+            Destroy(destructable.gameObject);
         }
+    }
+
+    private void PlayerDie()
+    {
+        if (gameplayUI != null)
+            gameplayUI.SetActive(false);
+
+        if (gameOverUI != null)
+            gameOverUI.SetActive(true);
+
+        Time.timeScale = 0f;
+
+        Destroy(gameObject);
     }
 }
